56409d6eb9548e521d15593c91b2a19dc830ff65
DineshK90 <111674856+DineshK90@users.noreply.github.com>
2022-11-17 01:48:06 +0800

    typo edit

--- src/data/experience.js
+++ src/data/experience.js
@@ -3,7 +3,7 @@ export const ExperienceData = [
         id: 1,
         title: "Medical Officer",
         experience: "Ministry Of Health, Malaysia",
-        year: 2016-2019,
+        year: '2016 - 2019',
     },
 //     {
 //         id: 2,
